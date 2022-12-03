766fc9f90c50abf5e71e383ba249ced867ddcbe0
MariaS111 <mariasviridova470@gmail.com>
2022-12-03 14:32:11 +0300

    new

--- HopfieldNN/controller.py
+++ HopfieldNN/controller.py
@@ -10,7 +10,7 @@ class Converter:
         res = []
         patterns, pattern = [], []
         for i in self.files:
-            with open(f'sourse_patterns/{i}', 'r') as file:
+            with open(f'source_patterns/{i}', 'r') as file:
                 res.append(file.read())
         for i in res:
             pattern = i.replace('.', '-1 ').replace('+', '1 ')
@@ -18,18 +18,21 @@ class Converter:
             patterns.append(pattern)
         return patterns
 
-    def read_noisy_pattern(self):
-        i = 'F2'
+    @staticmethod
+    def read_noisy_pattern():
+        i = 'T1'
         with open(f'noisy_patterns/{i}', 'r') as file:
             res = file.read()
         result = res.replace('.', '-1 ').replace('+', '1 ')
         result = [[int(i)] for j in result.split('\n') for i in j.split()]
         return result
 
-    def reverse_converter(self, data):
+    @staticmethod
+    def reverse_converter(data):
         return data.reshape(10, 10)
 
-    def made_result_pattern(self, data):
+    @staticmethod
+    def made_result_pattern(data):
         res = data.tolist()
         for i in range(len(res)):
             res[i] = [['+' if i[j] == 1 else '.' for j in range(len(i))] for i in res]
@@ -43,18 +46,17 @@ class NeuralNetwork:
         for i in range(len(self.patterns)):
             self.patterns[i] = np.array(self.patterns[i])
 
-    def function_of_activation(self, num):
+    @staticmethod
+    def function_of_activation(num):
         return -1 if num < 0 else 1
 
     def multiply(self, weight, matrix):
         matrix = np.array(matrix)
         sign = np.vectorize(self.function_of_activation)
         result = sign(np.dot(weight, matrix))
-        #c = Converter()
         for i in range(12):
             result = sign(np.dot(weight, self.asynchronous_execution(result, matrix)))
         return result
-        #c.reverse_converter(result)
 
     def create_weight_matrix(self):
         data = self.patterns
@@ -64,7 +66,8 @@ class NeuralNetwork:
             weights += ups / (np.dot(data[i].T, data[i]) - np.dot(np.dot(data[i].T, weights), data[i]))
         return weights
 
-    def asynchronous_execution(self, new_data, old_data):
+    @staticmethod
+    def asynchronous_execution(new_data, old_data):
         result = copy.deepcopy(new_data)
         for i in range(len(new_data)):
             if i < len(new_data) // 2:
--- HopfieldNN/main.py
+++ HopfieldNN/main.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     resulted_pattern = converter.reverse_converter(neuralnetwork.multiply(neuralnetwork.create_weight_matrix(), converter.read_noisy_pattern()))
     for i in converter.made_result_pattern(resulted_pattern)[0]:
         print(i)
+        
